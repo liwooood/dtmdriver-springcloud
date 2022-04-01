73b5736cf6595a9c47f13084d8876f7d864d2c47
KuiLiao <horsekiao@gmail.com>
2022-04-02 00:01:18 +0800

    update

--- httpdriver/http_driver.go
+++ httpdriver/http_driver.go
@@ -4,5 +4,5 @@ type HttpDriver interface {
 	// RegisterHttpResolver register the http resolver to handle custom scheme
 	RegisterHttpResolver()
 	// RegisterHttpService register endpoint to target with some params, such as username, password.
-	RegisterHttpService(target string, endpoint string, options map[string]string) error
+	RegisterHttpService(target string, endpoint string, options map[string]string, paths []string) error
 }
